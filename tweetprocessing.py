fdd0f32aad1a4518418d814255ca3a9231c4842f
Damon C. Roberts <damon.roberts-1@colorado.edu>
2020-01-08 11:06:47 -0700

    Create tweetprocessing.py

---
+++ tweetprocessing.py
@@ -0,0 +1,7 @@
+#--------------------------
+#Run Twitter-Genderd Reactions....py file first
+#-------------------------
+#pip install tweet-preprocessor
+import preprocessor as p
+import tweetpull
+#Cleaning the tweet
