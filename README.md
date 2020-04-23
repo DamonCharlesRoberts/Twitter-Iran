fcb8c46599e3d66dfb2c45aaa4bd99245c42261b
Damon C. Roberts <damon.roberts-1@colorado.edu>
2020-04-23 13:20:27 -0600

    Changes

--- README.md
+++
@@ -1,3 +0,0 @@
-# Twitter
-</h2><i>Twitter Responses to Iran</h2></i>
-<p>Please note that this is a work in progress. Soon, I will add files that do NLP and unsupervised ML to this repository.</p>
--- tweetprocessing.py
+++
@@ -1,8 +0,0 @@
-#--------------------------
-#Run Twitter-Genderd Reactions....py file first
-#-------------------------
-#pip install tweet-preprocessor
-import preprocessor as p
-import tweetpull
-#Cleaning the tweet
-p.clean(tweetpull.iran_tweets)
